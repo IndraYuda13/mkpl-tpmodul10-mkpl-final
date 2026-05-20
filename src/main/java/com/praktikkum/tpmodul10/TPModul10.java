ff8a0516506a4ee510c710c585b9ef5549a2839c
Indra Yuda Adi Saputra <indrayuda@student.telkomuniversity.ac.id>
2026-05-20 12:19:09 +0700

    fix: integrate Maven build with Java 21

--- src/main/java/com/praktikkum/tpmodul10/TPModul10.java
+++ src/main/java/com/praktikkum/tpmodul10/TPModul10.java
@@ -1,26 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.praktikkum.tpmodul10;
 
-/**
- *
- * @author Rendy
- */
 public class TPModul10 {
 
     public static void main(String[] args) {
-        Serangga S = new Semut("hitam", 60, 80);
-        System.out.println("Warna Semut\t: " + S.getWarna());
-        S.info();
-        S.gerak(new int[]{70, 90});
-        S.info();
-        Serangga L = new Lebah("kuning", 20,30,40);
-        System.out.println("Warna Lebah\t: " + L.getWarna());
-        L.terbang(55,65,75);
-        L.info();
-        L.gerak(new int[]{25, 35, 45});
-        L.info();
+        Serangga semut = new Semut("hitam", 60, 80);
+        System.out.println("Warna Semut: " + semut.getWarna());
+        semut.info();
+        semut.gerak(new int[]{70, 90});
+        semut.info();
+
+        Lebah lebah = new Lebah("kuning", 20, 30, 40);
+        System.out.println("Warna Lebah: " + lebah.getWarna());
+        lebah.terbang(55, 65, 75);
+        lebah.info();
+        lebah.gerak(new int[]{25, 35, 45});
+        lebah.info();
     }
 }
